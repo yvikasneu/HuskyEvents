39e141780dff99f22bd5244df741fca308682306
likhitha1519 <145051890+likhitha1519@users.noreply.github.com>
2023-12-03 21:49:40 -0500

    Added comment model

---
+++ src/models/Comment.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Likhitha
+ */
+public class Comment {
+    private int id; 
+    private int event_id; 
+    private String author; 
+    private String comment; 
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the event_id
+     */
+    public int getEvent_id() {
+        return event_id;
+    }
+
+    /**
+     * @param event_id the event_id to set
+     */
+    public void setEvent_id(int event_id) {
+        this.event_id = event_id;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the comment
+     */
+    public String getComment() {
+        return comment;
+    }
+
+    /**
+     * @param comment the comment to set
+     */
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
